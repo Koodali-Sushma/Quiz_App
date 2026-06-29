814cdd98b7da5e27a5c40dc9085bfa9e849be02f
Sushma Koodali <sushma.gargya@outlook.com>
2026-06-30 00:28:16 +0200

    Recapproject2 task 1 implemented

---
+++ index.js
@@ -0,0 +1,30 @@
+/////Task 1
+
+const bookmarkButton = document.querySelector('[data-js="bookmarkButton"]');
+
+let bookmarkActive = true;
+bookmarkButton.addEventListener("click", () => {
+  console.log("You clicked me!!!!!");
+  event.preventDefault();
+  if (bookmarkActive) {
+    bookmarkButton.style.color = "var(--black)";
+    console.log("I am here");
+  } else {
+    bookmarkButton.style.color = "var(--white)";
+  }
+  bookmarkActive = !bookmarkActive;
+});
+
+/////Task 2
+
+const answerButton = document.querySelector('[data-js="answerButton"');
+const answerDisplay = document.querySelector('[data-js="answerDisplay"');
+answerButton.addEventListener("click", () => {
+  if (answerDisplay.hasAttribute("hidden")) {
+    answerButton.textContent = "Hide Answer";
+    answerDisplay.removeAttribute("hidden");
+  } else {
+    answerDisplay.setAttribute("hidden", "");
+    answerButton.textContent = "Show Answer";
+  }
+});
